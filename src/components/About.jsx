08b3c7c31a1d141b34f17b63f5b7c4edd9fae7ca
arthaz1245 <sergiomendozap96@gmail.com>
2023-06-05 15:34:43 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
       </h1>
       <p className="text-center py-8 ">
         Hi, I'm Sergio. I've always been really interested in the web
-        development.I've always wanted to developmy own website and be willing
-        to learn more and more everyday Here I want to share with you more about
+        development. I've always wanted to develop my own website and be willing
+        to learn more and more everyday. I want to share with you more about
         my profile
       </p>
       <div className="flex justify-center">
